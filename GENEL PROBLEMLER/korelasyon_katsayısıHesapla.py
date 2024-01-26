9903e04948566e1fea62ea7838e3b48c60c2cf9a
MUSTAFA ÖZDEMİR <105823315+mustafa0zdemir@users.noreply.github.com>
2024-01-27 02:14:25 +0300

    Add files via upload

---
+++ GENEL PROBLEMLER/korelasyon_katsayısıHesapla.py
@@ -0,0 +1,48 @@
+x = [1,2,3,4,5,6,7]
+y = [9,8,10,12,11,13,14]
+
+
+toplamA = 0
+for i in range(7):
+    carpım = x[i] * y[i]
+    toplamA += carpım
+ortalamaA = toplamA /7
+print(ortalamaA)
+
+
+toplamY = 0
+toplamX = 0
+for i in range(7):
+    toplamX += x[i]
+    toplamY += y[i]
+
+ortX = toplamX /7
+ortY = toplamY /7
+
+pay = ortalamaA - (ortX * ortY)
+
+xkaretoplam = 0
+ykaretoplam = 0
+for i in range(7):
+    xkaretoplam += x[i]**2
+    ykaretoplam += y[i]**2
+
+ykareort = ykaretoplam / 7
+xkareort = xkaretoplam / 7
+print(xkareort)
+
+payda = ((xkareort - (ortX**2))**0.5) * ((ykareort - (ortY**2))**0.5)
+
+korelasyonKatsayısı = pay / payda
+print(korelasyonKatsayısı)
+
+
+
+
+
+
+
+
+
+
+
