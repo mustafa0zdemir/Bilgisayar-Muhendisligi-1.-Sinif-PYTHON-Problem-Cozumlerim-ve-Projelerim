ca31c09fc70039870560d0e69129fc77125f8483
MUSTAFA ÖZDEMİR <105823315+mustafa0zdemir@users.noreply.github.com>
2024-09-24 22:22:21 +0300

    Update sayidonusturme.py

--- GENEL PROBLEMLER/sayidonusturme.py
+++ GENEL PROBLEMLER/sayidonusturme.py
@@ -1,18 +1,18 @@
-#çift sayıları tek sayı yapan algoritma
-def TekSayiDonustur(N, sayilar):
+#çift sayıları sayının bir üst değeriyle değiştirerek tek sayı yapan algoritma
+def TekSayiDonustur(sayilar):
     degisen_sayilar = 0
 
-    for i in range(N):
+    for i in range(len(sayilar)-1):
         if sayilar[i] % 2 == 0:
             sayilar[i] += 1
             degisen_sayilar += 1
 
     if degisen_sayilar > 0:
         print("Yeni liste:", sayilar)
-        print(f"Toplamda {degisen_sayilar} adet yer değiştirme yapıldı.")
+        print(f"Toplamda {degisen_sayilar} adet sayı değiştirildi.")
     else:
         print("Çift sayı bulunamadı.")
 
 
 
-TekSayiDonustur(10,[1,2,3,4,5,6,7,8,9,10])
+TekSayiDonustur([1,2,3,4,5]) //Test değeri
